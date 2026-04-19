6275b95703a57fb4dac59ae03aab74c4c4083e36
Bing-ruuu <778634974@qq.com>
2026-04-19 22:47:54 +0800

    彻底删除db.create_all()，修复线上500错误

--- app.py
+++ app.py
@@ -182,6 +182,4 @@ def get_dashboard_stats():
 
 # ===================== 启动 =====================
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)
